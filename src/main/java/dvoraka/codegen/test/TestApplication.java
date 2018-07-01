89877afbbacb2b1db069dd456d9a0f0ed48b3db9
dvoraka <alen.dvorak@gmail.com>
2018-07-01 12:56:07 +0200

    Add JavaPoet example

--- src/main/java/dvoraka/codegen/test/TestApplication.java
+++ src/main/java/dvoraka/codegen/test/TestApplication.java
@@ -1,12 +1,42 @@
 package dvoraka.codegen.test;
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.lang.model.element.Modifier;
 
 @SpringBootApplication
 public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+        return (args) -> {
+
+            MethodSpec main = MethodSpec.methodBuilder("main")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .returns(void.class)
+                    .addParameter(String[].class, "args")
+                    .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+                    .build();
+
+            TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                    .addMethod(main)
+                    .build();
+
+            JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                    .build();
+
+            javaFile.writeTo(System.out);
+        };
     }
 }
